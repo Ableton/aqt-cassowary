33260f0fdc9e81c9fecbb337015b021136ece36f
jbo <jbo@ableton.com>
2015-08-25 19:05:57 +0200

    Log when a variable gets an update from the solver

--- src/aqt/cassowary/Variable.cpp
+++ src/aqt/cassowary/Variable.cpp
@@ -27,9 +27,13 @@ Variable::Variable(QQuickItem* pParent)
   : Contextual(pParent)
   , mVariable([this] (double value) {
     if (!rhea::near_zero(value - mLastValue)) {
+      log("    ... updated: ", mVariable, value);
       mLastValue = value;
       auto ctx = context();
-      auto notification = [this, value] { Q_EMIT valueChanged(value); };
+      auto notification = [this, value] {
+        log("    ... notify: ", mVariable, value);
+        Q_EMIT valueChanged(value);
+      };
       if (ctx) {
         ctx->notify(mVariable, guarded(notification));
       } else {
